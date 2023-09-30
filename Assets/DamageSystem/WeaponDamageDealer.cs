b4ebb398678a762cb300e39a117879a271cf85bc
MBrosik <michal.bros1@gmail.com>
2023-10-01 01:54:14 +0200

    Done my friend

--- Assets/DamageSystem/WeaponDamageDealer.cs
+++ Assets/DamageSystem/WeaponDamageDealer.cs
@@ -1,11 +1,19 @@
-using System;
+﻿using System;
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 namespace DamageSystem {
     public class WeaponDamageDealer : MonoBehaviour, IActiveDamageDealer {
         [SerializeField] private Weapon weapon;
+        [SerializeField] private ParticleSystem particleSystem;
+
+        private float lastTimeDamageGot = 0;
+
         public float GetDamage() {
+            particleSystem.gameObject.SetActive(true);
+            lastTimeDamageGot = Time.time;
+
             return weapon.GetDamage();
         }
 
@@ -13,6 +21,15 @@ namespace DamageSystem {
             if (!weapon) return;
 
             weapon.Attack();
+        }
+
+        IEnumerator CheckParticleSystem() {
+            while (true) {
+                if (Time.time - lastTimeDamageGot > 1000) {
+                    particleSystem.gameObject.SetActive(false);
+                }
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
